2332a75816c89b9a9d71024ff3a36d83c9ac7fb1
lost3002 <995562615@qq.com>
2019-11-19 09:22:49 +0800

    添加修改分类

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,16 @@
 import ajax from './ajax'
 import {message} from 'antd'
 import jsonp from 'jsonp'
-
-export const reqLogin = (username, password) => ajax('/login', { username, password }, 'POST')
-
-export const reqAddUser = (user) => ajax('/manage/user/add', user, 'POST')
+const BASE = ''
+export const reqLogin = (username, password) => ajax(BASE+'/login', { username, password }, 'POST')
+//添加用户
+export const reqAddUser = (user) => ajax(BASE+'/manage/user/add', user, 'POST')
+//获取一级二级分类列表
+export const reqCategorys = (parentId) => ajax(BASE+'/manage/category/list',{parentId})
+//添加分类
+export const reqAddCategory = ({parentId, categoryName}) => ajax(BASE+'/manage/category/add',{parentId, categoryName},'POST')
+//更新分类
+export const reqUpdateCategory = (categoryId, categoryName) => ajax(BASE+'/manage/category/update',{categoryId, categoryName},'POST')
 
 // 天气请求
 export const reqWeather = (city) => {
--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -1,40 +1,23 @@
 import React, { Component } from 'react';
-import { Card, Table, Button, } from 'antd'
+import { Card, Table, Button, message, Icon, Modal } from 'antd'
 
 import LinkButton from '../../components/link-button'
+import { reqCategorys } from '../../api'
+import AddFrom from './add-form' 
 
 class Category extends Component {
 
+    state = {
+        loading: false,
+        categorys: [],
+        subCategorys: [],
+        parentId: '0',
+        parentName: '',
+        visible: 0
+    }
 
-
-
-
-    render() {
-
-        const dataSource = [
-            {
-                "parentId": "0",
-                "_id": "5c2ed631f352726338607046",
-                "name": "分类001",
-                "__v": 0
-            },
-            {
-                "parentId": "0",
-                "_id": "5c2ed647f352726338607047",
-                "name": "分类2",
-                "__v": 0
-            },
-            {
-                "parentId": "0",
-                "_id": "5c2ed64cf352726338607048",
-                "name": "1分类3",
-                "__v": 0
-            }
-        ]
-
-
-
-        const columns = [
+    initColumns = () => {
+        this.columns = [
             {
                 title: '分类',
                 dataIndex: 'name',
@@ -43,25 +26,121 @@ class Category extends Component {
                 title: '操作',
                 width: 300,
                 dataIndex: '',
-                render: () => (
+                render: (category) => (
                     <span>
-                        <LinkButton>修改</LinkButton>
-                        <LinkButton>查看子分类</LinkButton>
+                        <LinkButton onClick={this.showUpdate}>修改</LinkButton>
+                        {
+                            this.state.parentId === '0'
+                                ?
+                                <LinkButton onClick={() => this.showSubCategorys(category)}>查看子分类</LinkButton>
+                                :
+                                null
+                        }
                     </span>
                 )
             },
         ]
+    }
 
-        const title = '商品分类'
+
+
+    getCategorys = async () => {
+        this.setState({ loading: true })
+        const { parentId } = this.state
+        const result = await reqCategorys(parentId)
+        this.setState({ loading: false })
+        console.log('cc', result)
+        if (result.status === 0) {
+            const categorys = result.data
+
+            parentId === '0' ? this.setState({ categorys }) : this.setState({ subCategorys: categorys })
+        } else {
+            message.error('categorys请求错误')
+        }
+    }
+
+    showSubCategorys = (category) => {
+        this.setState({ parentId: category._id, parentName: category.name }, () => {
+            this.getCategorys()
+        })
+    }
+    showCategorys = () => {
+        this.setState({
+            subCategorys: [],
+            parentId: '0',
+            parentName: ''
+        })
+    }
+
+    handleCancel = () => {
+        this.setState({visible : 0})
+    }
+    addCategory = () => {
+
+    }
+    showAdd = () => {
+        this.setState({visible: 1})
+    }
+    updateCategory = () => {
+
+    }
+    showUpdate = () => {
+        this.setState({visible: 2})
+    }
+
+    componentWillMount() {
+        this.initColumns()
+    }
+    componentDidMount() {
+        this.getCategorys()
+    }
+
+
+    render() {
+        const { categorys, loading, parentId, subCategorys, parentName, visible } = this.state
+
+        const title = parentId === '0' ? '一级商品分类' : (
+            <span>
+                <LinkButton onClick={this.showCategorys}>一级商品分类</LinkButton>
+                <Icon type='arrow-right'></Icon>
+                <span>{parentName}</span>
+            </span>
+        )
+
         const extra = (
-            <Button type="primary" shape="round" icon="plus">
+            <Button type="primary" shape="round" icon="plus" onClick={this.showAdd}>
                 添加分类
             </Button>
         )
         return (
             <div>
                 <Card title={title} extra={extra} >
-                    <Table dataSource={dataSource} columns={columns} rowKey='_id' bordered />
+                    <Table
+                        dataSource={parentId === '0' ? categorys : subCategorys}
+                        columns={this.columns}
+                        rowKey='_id'
+                        bordered
+                        loading={loading}
+                        pagination={{ defaultPageSize: 5, showQuickJumper: true }} />
+
+                    <Modal
+                        title="添加分类"
+                        visible={visible === 1}
+                        onOk={this.addCategory}
+                        onCancel={this.handleCancel}
+                    >
+                        <AddFrom />
+
+                    </Modal>
+                    <Modal
+                        title="修改分类"
+                        visible={visible === 2}
+                        onOk={this.updateCategory}
+                        onCancel={this.handleCancel}
+                    >
+                        <p>Some contents...</p>
+
+                    </Modal>
                 </Card>
             </div>
         );
